786fbbb0d4d9632e7c15429434d2ed49f85899d9
sowmya <sowmya.kol@gmail.com>
2019-10-26 13:37:14 -0700

    chunk Array

--- chunkArray.js
+++ chunkArray.js
@@ -3,7 +3,38 @@
 //ex. chunkArray([1,2,3,4,5,6,7],3) === [[1,2,3],[4,5,6].[7]]
 //ex. chunkarray([1,2,3,4,5,6,7],2) === [[1,2],[3,4],[5,6],[7]]
 
-function chunkArray(arr,len) {
+function chunkArray(arr, len) {
+  //SOLUTION 1
+  let chunkArray = [];
+  let i = 0;
 
+  while (i < arr.length) {
+    //slice from index to index + chunkLength
+    //and push into chunkArray
+    chunkArray.push(arr.slice(i, i + len));
+    //increment by chunk length
+    i = i + len;
+  }
+  return chunkArray;
 
+  //SOLUTION 2
+  // initalize the chunk array with
+  //   const chunkArray = [];
+
+  //   //loop through array
+  //   arr.forEach(function(val) {
+  //     //get last element
+  //     const last = chunkArray[chunkArray.length - 1];
+
+  //     //check if last is undefined and is last length is equal to chunk length
+  //     if (!last || last.length === len) {
+  //       chunkArray.push([val]);
+  //     } else {
+  //       last.push(val);
+  //     }
+  //     // console.log(chunkArray);
+  //   });
+  //   return chunkArray;
 }
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2));
