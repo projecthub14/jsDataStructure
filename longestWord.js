210a0a59e05bc5c3b829495ee8c80699610ccb00
sowmya <sowmya.kol@gmail.com>
2019-10-26 12:56:21 -0700

    longest word

---
+++ longestWord.js
@@ -0,0 +1,19 @@
+function longestWord(sen) {
+  //create filter array
+  const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+  //Sort by length
+  const sorted = wordArr.sort((a, b) => b.length - a.length);
+
+  //if multiple words , put into array
+  const longestWordArr = sorted.filter(
+    word => word.length === sorted[0].length
+  );
+
+  //Check if more than one array val
+  if (longestWordArr.length === 1) {
+    return longestWordArr.toString();
+  }
+  return longestWordArr;
+}
+console.log(longestWord("hello there , my name is Brad"));
