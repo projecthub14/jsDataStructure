aaccaaba1e06d8cecef08167a9c8a11d296249ea
sowmya <sowmya.kol@gmail.com>
2019-10-28 15:50:07 -0700

    summ of all primes

---
+++ sumAllPrimes.js
@@ -0,0 +1,25 @@
+//CHALLENGE 2 : SUM ALL PRIMES
+//PASS in a number to loop up to and add all of the prime numbers .
+// A prime number is a whole number greater than 1 whose only factors are
+// 1 and itself
+//ex. sumAllPrimes(10) == 17
+
+function sumAllPrimes(num) {
+  let total = 0;
+  function checkForPrime(i) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  for (let i = 2; i <= 10; i++) {
+    if (checkForPrime(i)) {
+      total += i;
+    }
+  }
+  return total;
+}
+
+console.log(sumAllPrimes(10));
